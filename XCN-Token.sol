0e1267974a8e143d02b9a112202acad5f13e72b7
ChainEngineer1 <74755843+ChainEngineer1@users.noreply.github.com>
2022-03-07 00:38:25 +1000

    update base token

--- XCN-Token.sol
+++ XCN-Token.sol
@@ -476,7 +476,7 @@ contract ChainToken is ERC20Mintable, ERC20Detailed {
     uint8 public constant DECIMALS = 18;
     uint256 public constant INITIAL_SUPPLY = 20000000000 * (10 ** uint256(DECIMALS));
     uint256 public constant MAX_SUPPLY = 68895442185 * (10 ** uint256(DECIMALS));
-    address public constant MINT_BASE = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+    address public constant MINT_BASE = 0x41C37A4683d6a05adB31c39D71348A8403B13Ca9;
 
     /// @notice A record of each accounts delegate
     mapping (address => address) public delegates;
